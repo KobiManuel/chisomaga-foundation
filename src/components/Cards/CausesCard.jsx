f51119711b751dd05bae4ad579848ad118ab6db4
KobiManuel <Emmakobi91@gmail.com>
2022-12-22 23:04:39 +0100

    feat(New Component): creadted CausesCard_data component

--- src/components/Cards/CausesCard.jsx
+++ src/components/Cards/CausesCard.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import Wrapper from '../UI/Wrapper'
+import {CausesCard_data} from '../ComponentData/CausesCard_data'
 
 const CausesCard = ({image, subtitle, progress, raisedAmount, goal}) => {
   return (
-    <Wrapper>
-        <img  src={image} alt="/"/>
+    <Wrapper bg_color=' !flex !flex-col !gap-3'>
+        <figure className='body-font font-TomatoFont w-[300px]'>
+            <img src={image} alt="/" />
+        </figure>
+        <header>
+            <h5 className=' text-[var(--blue)] font-medium text-xl leading-[1.4]'>{subtitle}</h5>
+        </header>
+        <progress value='70' max="100">
+            70%
+        </progress>
+        <div className='flex justify-between'>
+            <p className=' text-[var(--blue)] text-sm'>{`Raised: ${raisedAmount}`}</p>
+            <p className=' text-[var(--blue)] text-sm'>{`Goal: ${goal}`}</p>
+        </div>
     </Wrapper>
   )
 }
---
+++ src/components/ComponentData/CausesCard_data.jsx
@@ -0,0 +1,52 @@
+import hero from '../../assets/hero.jpg'
+export const causesInfo = [
+  {
+    image: hero,
+    subtitle: "Ensure Education For Every Poor Child",
+    progress: "70%",
+    raisedAmount: "N100,000",
+    goal: "N200,000",
+  },
+  {
+    image: hero,
+    subtitle: "Providing Healthy Food For The Children",
+    progress: "25%",
+    raisedAmount: "N200,000",
+    goal: "N300,000",
+  },
+  {
+    image: hero,
+    subtitle: "Supply Drinking Water To The People",
+    progress: "50%",
+    raisedAmount: "N300,000",
+    goal: "N400,000",
+  },
+  {
+    image: hero,
+    subtitle: "Ensure Education For Every Poor Child",
+    progress: "70%",
+    raisedAmount: "N100,000",
+    goal: "N200,000",
+  },
+  {
+    image: hero,
+    subtitle: "Ensure Education For Every Poor Child",
+    progress: "70%",
+    raisedAmount: "N100,000",
+    goal: "N200,000",
+  },
+  {
+    image: hero,
+    subtitle: "Ensure Education For Every Poor Child",
+    progress: "70%",
+    raisedAmount: "N100,000",
+    goal: "N200,000",
+  },
+  {
+    image: hero,
+    subtitle: "Ensure Education For Every Poor Child",
+    progress: "70%",
+    raisedAmount: "N100,000",
+    goal: "N200,000",
+  },
+];
