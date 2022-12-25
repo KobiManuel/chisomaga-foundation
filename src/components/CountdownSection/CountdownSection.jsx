0d15e6ea91a624a00a1f82cbab0eb2fb3be6fa7f
KobiManuel <Emmakobi91@gmail.com>
2022-12-26 00:42:26 +0100

    style: removed margin bottom on Countdown component

--- src/components/CountdownSection/CountdownSection.jsx
+++ src/components/CountdownSection/CountdownSection.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import countdown_bg from '../../assets/hero6.jpg'
 const CountdownSection = () => {
   return (
-    <div className=' mb-10' style={{
+    <div style={{
         backgroundImage: 'url('+countdown_bg+')',
         backgroundSize: "cover",
         backgroundRepeat: "no-repeat",
