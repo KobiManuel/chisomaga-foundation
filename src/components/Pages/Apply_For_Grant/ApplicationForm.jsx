12c6f45e6f15308eb2554c9acca9830a93e7a534
KobiManuel <Emmakobi91@gmail.com>
2023-01-05 23:01:56 +0100

    feat(New import): imported NotificationBar into applicationForm page

--- src/components/Pages/Apply_For_Grant/ApplicationForm.jsx
+++ src/components/Pages/Apply_For_Grant/ApplicationForm.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Wrapper from '../../UI/Wrapper'
+import NotificationBar from '../Landing_Page/Navbar/NotificationBar'
 
 
 const ApplicationForm = () => {
   return (
     <>
+    <NotificationBar />
        <Wrapper>
         
        </Wrapper>
