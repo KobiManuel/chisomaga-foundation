ec3a67eab93358908e299cad0ad378974a800bda
KobiManuel <Emmakobi91@gmail.com>
2023-01-06 23:59:02 +0100

    feat: formatted ChangeHumanity section

--- src/components/Pages/Landing_Page/ChangeHumanity/ChangeHumanitySection.jsx
+++ src/components/Pages/Landing_Page/ChangeHumanity/ChangeHumanitySection.jsx
@@ -1,29 +1,36 @@
-import React from 'react'
-import Wrapper from '../../../UI/Wrapper'
-import welcome_bg from '../../../../assets/hero6.jpg'
-import Button from '../Button/Button'
-import styles from '../Button/TransparentBtn.module.css'
+import React from "react";
+import Wrapper from "../../../UI/Wrapper";
+import welcome_bg from "../../../../assets/hero6.jpg";
+import Button from "../Button/Button";
+import styles from "../Button/TransparentBtn.module.css";
 const ChangeHumanitySection = () => {
   return (
     <Wrapper>
-        <div className='body-font font-TomatoFont max-w-[80%] mx-auto px-4 flex justify-between items-center max-[665px]:justify-center gap-3' style={{
-      backgroundImage: 'url('+welcome_bg+')',
-      backgroundSize: "cover",
-      backgroundRepeat: "no-repeat",
-      backgroundPosition: "center center",
-      backgroundAttachment: "fixed",
-      maxWidth: "85%",
-      height: "265px"
-    }}>
-     <div className=' max-w-[428.33px] max-h-[105px] pl-8 max-[665px]:pl-0'>
-        <p className=' capitalize text-white font-semibold text-[35px] max-[665px]:text-[20px]'>Let's change the world with humanity</p>
-     </div>
-     <div className=' w-fit pr-8 max-[665px]:pr-0 max-[410px]:hidden'>
-     <Button type="button" className={styles.button}>Donate</Button>
-     </div>
+      <div
+        className="body-font font-TomatoFont max-w-[80%] mx-auto px-4 flex justify-between items-center max-[665px]:justify-center gap-3"
+        style={{
+          backgroundImage: "url(" + welcome_bg + ")",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center center",
+          backgroundAttachment: "fixed",
+          maxWidth: "85%",
+          height: "265px",
+        }}
+      >
+        <div className=" max-w-[428.33px] max-h-[105px] pl-8 max-[665px]:pl-0">
+          <p className=" capitalize text-white font-semibold text-[35px] max-[665px]:text-[20px]">
+            Let's change the world with humanity
+          </p>
         </div>
+        <div className=" w-fit pr-8 max-[665px]:pr-0 max-[410px]:hidden">
+          <Button type="button" className={styles.button}>
+            Donate
+          </Button>
+        </div>
+      </div>
     </Wrapper>
-  )
-}
+  );
+};
 
-export default ChangeHumanitySection
+export default ChangeHumanitySection;
