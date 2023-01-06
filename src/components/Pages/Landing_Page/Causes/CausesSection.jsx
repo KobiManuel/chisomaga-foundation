37e25bf64ee8ba8ee81cb4bbd27967d0354e73a1
KobiManuel <Emmakobi91@gmail.com>
2023-01-06 23:59:25 +0100

    feat: formatted document

--- src/components/Pages/Landing_Page/Causes/CausesSection.jsx
+++ src/components/Pages/Landing_Page/Causes/CausesSection.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import CausesCard from '../../../Cards/CausesCard'
-import Wrapper from '../../../UI/Wrapper'
+import React from "react";
+import CausesCard from "../../../Cards/CausesCard";
+import Wrapper from "../../../UI/Wrapper";
 import { causesInfo } from "../../../ComponentData/CausesCard_data";
-import Slider from 'react-slick';
+import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const CausesSection = () => {
-
   const settings = {
     dots: true,
     infinite: true,
@@ -23,8 +22,8 @@ const CausesSection = () => {
           slidesToShow: 2,
           slidesToScroll: 2,
           infinite: true,
-          dots: true
-        }
+          dots: true,
+        },
       },
       {
         breakpoint: 799,
@@ -32,35 +31,42 @@ const CausesSection = () => {
           slidesToShow: 1,
           slidesToScroll: 1,
           infinite: true,
-          dots: true
-        }
-      }
-    ]
+          dots: true,
+        },
+      },
+    ],
   };
 
   return (
-    <Wrapper id="causes" bg_color=' !bg-[#f1f6f9]'>
-       <header id='about' className='body-font font-TomatoFont w-[570px] max-[580px]:w-[80%] h-[270px] mx-auto text-center'>
-        <h5 className='text-[var(--primary)] text-lg leading-10'>Our Causes You Can See</h5>
-        <h2 className=' capitalize text-[var(--blue)] max-[580px]:text-[30px] text-5xl leading-[1.4]'>Explore the latest causes we've worked on</h2>
-       </header>
-       <Slider  {...settings} className=' overflow-hidden px-4  flex flex-wrap justify-between items-center max-w-[1100px] mx-auto'>
-        {causesInfo.map(
-          ({ image, subtitle, progress, raisedAmount, goal }) => (
-            <CausesCard
-              image={image}
-              subtitle={subtitle}
-              progress={progress}
-              raisedAmount={raisedAmount}
-              goal={goal}
-              key={Math.random().toString()}
-            />
-          )
-        )}
-      </Slider> 
-     
+    <Wrapper id="causes" bg_color=" !bg-[#f1f6f9]">
+      <header
+        id="about"
+        className="body-font font-TomatoFont w-[570px] max-[580px]:w-[80%] h-[270px] mx-auto text-center"
+      >
+        <h5 className="text-[var(--primary)] text-lg leading-10">
+          Our Causes You Can See
+        </h5>
+        <h2 className=" capitalize text-[var(--blue)] max-[580px]:text-[30px] text-5xl leading-[1.4]">
+          Explore the latest causes we've worked on
+        </h2>
+      </header>
+      <Slider
+        {...settings}
+        className=" overflow-hidden px-4  flex flex-wrap justify-between items-center max-w-[1100px] mx-auto"
+      >
+        {causesInfo.map(({ image, subtitle, progress, raisedAmount, goal }) => (
+          <CausesCard
+            image={image}
+            subtitle={subtitle}
+            progress={progress}
+            raisedAmount={raisedAmount}
+            goal={goal}
+            key={Math.random().toString()}
+          />
+        ))}
+      </Slider>
     </Wrapper>
-  )
-}
+  );
+};
 
-export default CausesSection
+export default CausesSection;
