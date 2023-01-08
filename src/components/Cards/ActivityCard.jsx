280ca972e51b54210595a0833261961f10b2cffb
KobiManuel <Emmakobi91@gmail.com>
2023-01-08 23:40:32 +0100

    style: formatted document

--- src/components/Cards/ActivityCard.jsx
+++ src/components/Cards/ActivityCard.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import styles from './ActivityCard.module.css'
+import React from "react";
+import styles from "./ActivityCard.module.css";
 
-const ActivityCard = ({children, className}) => {
+const ActivityCard = ({ children, className }) => {
   return (
-    <div className={` shadow-2xl p-[40px] w-[80%] justify-center  lg:h-[400px] lg:w-[351.38px] max-[1040px]:w-[80%] max-[1040px]:h-[300px] rounded-[24px]  flex flex-col items-center ${styles.card} ${className}` }>
+    <div
+      className={` shadow-2xl p-[40px] w-[80%] justify-center  lg:h-[400px] lg:w-[351.38px] max-[1040px]:w-[80%] max-[1040px]:h-[300px] rounded-[24px]  flex flex-col items-center ${styles.card} ${className}`}
+    >
       {children}
     </div>
-  )
-}
+  );
+};
 
-export default ActivityCard
+export default ActivityCard;
