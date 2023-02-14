9fa52eac1ada4cb964ca8d805d9656d5cbac9726
KobiManuel <Emmakobi91@gmail.com>
2023-02-14 08:38:24 +0100

    style: spellcheck correction

--- src/components/Pages/Landing_Page/Navbar/NotificationBar.jsx
+++ src/components/Pages/Landing_Page/Navbar/NotificationBar.jsx
@@ -9,10 +9,10 @@ const NotificationBar = () => {
       <div className="body-font font-TomatoFont h-14 bg-[var(--primary)] py-5">
         <div className=" px-4 flex h-7 justify-between  max-[883px]:justify-center mt-0 mx-auto md:max-w-[1100px]">
           <p className="text-white text-sm  pt-0 pr-3 h-5 border-r-2  max-[883px]:hidden">
-            Phone: +234 3 728 5562
+            Phone: +234 703 728 5562
           </p>
           <p className="text-white text-sm pt-0  px-3 h-5 text-center border-r-2  max-[883px]:hidden">
-            Email: chisomaga-foundation@gmail.com.com
+            Email: thechisomagafoundation@gmail.com
           </p>
           <div className="flex justify-end gap-5 pl-3 cursor-pointer text-white">
             <FaTwitter className="  max-[883px]:hidden w-9 transform hover:-scale-x-100 transition duration-500 ease-in-out hover:text-[var(--blue)]" />
